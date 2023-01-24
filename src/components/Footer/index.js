4307e4d762591c37f33fec2f359f3c2b5eb0de2a
´´MARISA16´´ <marisamaysarog1992@gmail.com>
2023-01-24 19:27:17 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './styles.css'
 function Footer(props){
     return(
         <div className='footer'>
-        <p> Projeto produzida por Marisa António-2020 <a className='footer-link' target='blank' href='https://github.com/MARISA16'>    Sobre a autora</a></p>
+        <p>  Desenvolvida por Marisa António <a className='footer-link' target='blank' href='https://github.com/MARISA16'>    Sobre a autora</a></p>
         </div>
     )
 }
